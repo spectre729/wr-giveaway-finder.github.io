98f78578cbe4aefc4d7686a2fb47c0faed1037e1
spectre729 <dchen4761@gmail.com>
2026-08-25 10:59:18 -0400

    Added new assets and made some modifications with my own personal touch

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.media-frame').forEach((frame) => {
-  const video = frame.querySelector('video.media');
-  if (!video) return;
-  video.addEventListener('loadeddata', () => frame.classList.add('has-media'));
-});
